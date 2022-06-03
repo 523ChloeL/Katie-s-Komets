4adfbcb2609170e869d3609a24c65833c0e1b9fc
clara <92735915+523Cs@users.noreply.github.com>
2022-06-03 10:33:13 -0400

    Add files via upload

--- komets.js
+++ komets.js
@@ -1,17 +1,4 @@
-var icon = document.getElementById("icon");
-var icon1 = document.getElementById("a");
-var icon2 = document.getElementById("b");
-var icon3 = document.getElementById("c");
-var nav = document.getElementById('nav');
-var blue = document.getElementById("blue");
-
-icon.addEventListener('click', function() {
-  icon1.classList.toggle('a');
-  icon2.classList.toggle('c');
-  icon3.classList.toggle('b');
-  nav.classList.toggle('show');
-  blue.classList.toggle('slide');
-});
+\
 
 function clearName() {
 if (document.getElementById('emailField').value == 'Email Address'){
